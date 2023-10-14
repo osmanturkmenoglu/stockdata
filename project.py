7382f3d69f580e66a3667202bcde4872d7abcc74
osmeyn <32793687+osmeyn@users.noreply.github.com>
2023-10-13 21:04:56 -0600

    Update project.py

--- project.py
+++ project.py
@@ -47,6 +47,34 @@ if st.button("Get Data") and symbol:
         # Convert columns with large numbers to a readable format (in billions) for display
         cols_to_convert = ['totalAssets', 'totalLiab', 'totalStockholderEquity', 'totalCurrentLiabilities', 'intangibleAssets', 'totalCurrentAssets', 'netInvestedCapital', 'otherCurrentAssets', 'otherCurrentLiab', 'deferredLongTermLiab', 'commonStock', 'capitalStock', 'retainedEarnings', 'otherLiab', 'goodWill', 'otherAssets', 'cash', 'cashAndEquivalents', 'currentDeferredRevenue', 'netDebt', 'shortTermDebt', 'shortLongTermDebt', 'shortLongTermDebtTotal', 'otherStockholderEquity', 'propertyPlantEquipment', 'longTermInvestments', 'netTangibleAssets', 'shortTermInvestments', 'netReceivables', 'longTermDebt', 'inventory', 'accountsPayable', 'accumulatedOtherComprehensiveIncome', 'commonStockTotalEquity', 'retainedEarningsTotalEquity', 'nonCurrrentAssetsOther', 'nonCurrentAssetsTotal', 'capitalLeaseObligations', 'longTermDebtTotal', 'nonCurrentLiabilitiesOther', 'nonCurrentLiabilitiesTotal', 'capitalSurpluse', 'liabilitiesAndStockholdersEquity', 'cashAndShortTermInvestments', 'propertyPlantAndEquipmentGross', 'propertyPlantAndEquipmentNet', 'netWorkingCapital', 'commonStockSharesOutstanding']
         df[cols_to_convert] = df[cols_to_convert].applymap(lambda x: f'{float(x)/1e9:.2f}B' if pd.notnull(x) else x)
+
+        resp = client.get_fundamental_equity(symbol, filter_=f'Financials::Income_Statement::{frequency}')
+
+        # Convert the dictionary to a Pandas DataFrame
+        df = pd.DataFrame.from_dict(resp, orient='index')
+
+        # Drop the 'date' column as it's the same as the index
+        df = df.drop(columns=['date'])
+
+        # Rename the index to 'date'
+        df.index.name = 'date'
+
+        # Create a copy of the DataFrame for plotting
+        df_plot = df.copy()
+
+        # Convert columns with large numbers to a readable format (in billions) for display
+        # Identify columns that contain numerical values
+        cols_to_convert = [
+            'researchDevelopment', 'effectOfAccountingCharges', 'incomeBeforeTax', 'minorityInterest',
+            'netIncome', 'sellingGeneralAdministrative', 'grossProfit', 'reconciledDepreciation', 'ebit', 'ebitda',
+            'depreciationAndAmortization', 'nonOperatingIncomeNetOther', 'operatingIncome',
+            'otherOperatingExpenses', 'interestExpense', 'taxProvision', 'interestIncome',
+            'netInterestIncome', 'extraordinaryItems', 'nonRecurring', 'otherItems',
+            'incomeTaxExpense', 'totalRevenue', 'totalOperatingExpenses', 'costOfRevenue',
+            'totalOtherIncomeExpenseNet', 'discontinuedOperations', 'netIncomeFromContinuingOps',
+            'netIncomeApplicableToCommonShares', 'preferredStockAndOtherAdjustments'
+        ]
+        df[cols_to_convert] = df[cols_to_convert].applymap(lambda x: f'{float(x)/1e9:.2f}B' if pd.notnull(x) else x)
         
         # Streamlit App
         st.title('Financial Analysis')
