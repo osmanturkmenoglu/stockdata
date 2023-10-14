d7b57b11d3c49b352371dac7ad94258476c33a7b
osmeyn <32793687+osmeyn@users.noreply.github.com>
2023-10-13 20:29:13 -0600

    Update project.py

--- project.py
+++ project.py
@@ -31,24 +31,29 @@ if st.button("Get Data") and symbol:
     try:
         # Fetching the data based on user input and selected frequency
         resp = client.get_fundamental_equity(symbol, filter_=f'Financials::Balance_Sheet::{frequency}')
+        
+        # Convert the dictionary to a Pandas DataFrame
+        df = pd.DataFrame.from_dict(resp, orient='index')
 
+        # Drop the 'date' column as it's the same as the index
+        df = df.drop(columns=['date'])
 
-    # Convert the dictionary to a Pandas DataFrame
-    df = pd.DataFrame.from_dict(resp, orient='index')
+        # Rename the index to 'date'
+        df.index.name = 'date'
 
-    # Drop the 'date' column as it's the same as the index
-    df = df.drop(columns=['date'])
+        # Create a copy of the DataFrame for plotting
+        df_plot = df.copy()
 
-    # Rename the index to 'date'
-    df.index.name = 'date'
-
-    # Create a copy of the DataFrame for plotting
-    df_plot = df.copy()
-
-    # Convert columns with large numbers to a readable format (in billions) for display
-    cols_to_convert = ['totalAssets', 'totalLiab', 'totalStockholderEquity', 'totalCurrentLiabilities', 'intangibleAssets', 'totalCurrentAssets', 'netInvestedCapital', 'otherCurrentAssets', 'otherCurrentLiab', 'deferredLongTermLiab', 'commonStock', 'capitalStock', 'retainedEarnings', 'otherLiab', 'goodWill', 'otherAssets', 'cash', 'cashAndEquivalents', 'currentDeferredRevenue', 'netDebt', 'shortTermDebt', 'shortLongTermDebt', 'shortLongTermDebtTotal', 'otherStockholderEquity', 'propertyPlantEquipment', 'longTermInvestments', 'netTangibleAssets', 'shortTermInvestments', 'netReceivables', 'longTermDebt', 'inventory', 'accountsPayable', 'accumulatedOtherComprehensiveIncome', 'commonStockTotalEquity', 'retainedEarningsTotalEquity', 'nonCurrrentAssetsOther', 'nonCurrentAssetsTotal', 'capitalLeaseObligations', 'longTermDebtTotal', 'nonCurrentLiabilitiesOther', 'nonCurrentLiabilitiesTotal', 'capitalSurpluse', 'liabilitiesAndStockholdersEquity', 'cashAndShortTermInvestments', 'propertyPlantAndEquipmentGross', 'propertyPlantAndEquipmentNet', 'netWorkingCapital', 'commonStockSharesOutstanding']
-    df[cols_to_convert] = df[cols_to_convert].applymap(lambda x: f'{float(x)/1e9:.2f}B' if pd.notnull(x) else x)
-
+        # Convert columns with large numbers to a readable format (in billions) for display
+        cols_to_convert = ['totalAssets', 'totalLiab', 'totalStockholderEquity', 'totalCurrentLiabilities', 'intangibleAssets', 'totalCurrentAssets', 'netInvestedCapital', 'otherCurrentAssets', 'otherCurrentLiab', 'deferredLongTermLiab', 'commonStock', 'capitalStock', 'retainedEarnings', 'otherLiab', 'goodWill', 'otherAssets', 'cash', 'cashAndEquivalents', 'currentDeferredRevenue', 'netDebt', 'shortTermDebt', 'shortLongTermDebt', 'shortLongTermDebtTotal', 'otherStockholderEquity', 'propertyPlantEquipment', 'longTermInvestments', 'netTangibleAssets', 'shortTermInvestments', 'netReceivables', 'longTermDebt', 'inventory', 'accountsPayable', 'accumulatedOtherComprehensiveIncome', 'commonStockTotalEquity', 'retainedEarningsTotalEquity', 'nonCurrrentAssetsOther', 'nonCurrentAssetsTotal', 'capitalLeaseObligations', 'longTermDebtTotal', 'nonCurrentLiabilitiesOther', 'nonCurrentLiabilitiesTotal', 'capitalSurpluse', 'liabilitiesAndStockholdersEquity', 'cashAndShortTermInvestments', 'propertyPlantAndEquipmentGross', 'propertyPlantAndEquipmentNet', 'netWorkingCapital', 'commonStockSharesOutstanding']
+        df[cols_to_convert] = df[cols_to_convert].applymap(lambda x: f'{float(x)/1e9:.2f}B' if pd.notnull(x) else x)
+        
+        # Streamlit App
+        st.title('Financial Analysis')
+        st.write('This app displays the fundamental financial data.')
+        st.write(df)
+        st.line_chart(df_plot[['totalAssets', 'totalLiab', 'totalStockholderEquity']].astype(float))
+        
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
