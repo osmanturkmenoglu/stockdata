904474c5b5ee12a50aec4a856088cdda0ccdaaf3
osmanturkmenoglu <32793687+osmanturkmenoglu@users.noreply.github.com>
2023-11-02 18:41:53 -0600

    Update project.py

--- project.py
+++ project.py
@@ -106,7 +106,7 @@ def page1():
 
 
 # Define other pages as functions
-def page2():
+  def page2():
     st.title('Historical Multiples')
     st.write('This app displays the historical multiples data.')
     # Set default values for inputs
